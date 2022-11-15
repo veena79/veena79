1a83fce28385604649c661dc08410375cf184ec8
vk9646481@gmail.com <theerthakunjusss@gmail.com>
2022-11-15 11:41:02 +0530

    my first program

---
+++ large.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter the first number\n");
+scanf("%d\n",&a);
+printf("enter the second number\n");
+scanf("%d\n",&b);
+if(a>b)
+printf("a is larger\n");
+else
+printf("b is larger\n");
+}
